f64b130c80c8f7e6537ad43a5ea30cd1a0b9eb89
Farhan Omer <FOmer2@schools.nyc.gov>
2022-10-18 14:52:56 -0400

    Saved OOP Concept

--- OOConceptRevision/Program.cs
+++ OOConceptRevision/Program.cs
@@ -5,6 +5,52 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+            Employee objEmployee = new Employee();
+            objEmployee.Experience = 3;
+            objEmployee.CalculateSalary();
+            Console.ReadLine();
+
+            PermanentEmployee pEmployee = new PermanentEmployee();
+            pEmployee.Experience = 5;
+            pEmployee.CalculateSalary();
+            Console.ReadLine();
+
         }
+
+
+
+        public class Employee
+        {
+            public Employee(){
+            }
+
+            private int experience;
+
+            public int Experience 
+            {
+                get 
+                { return experience;
+                }
+                set {
+                    experience = value;
+                }
+            }
+
+            public void CalculateSalary()
+            {
+                int salary = Experience * 10;
+                Console.WriteLine("salary:{0}", salary);
+            }
+
+
+        }
+
+        public class PermanentEmployee: Employee
+        {
+
+        }
+
+
     }
 }
