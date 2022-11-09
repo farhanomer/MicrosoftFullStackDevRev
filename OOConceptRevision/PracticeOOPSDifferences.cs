6b234136c45599f0a5508caba7c01af545b74fd0
Farhan Omer <FOmer2@schools.nyc.gov>
2022-11-09 10:45:55 -0500

    Submit Interview Question

---
+++ OOConceptRevision/PracticeOOPSDifferences.cs
@@ -0,0 +1,28 @@
+﻿namespace OOConceptRevision
+{
+    internal class PracticeOOPSDifferences
+    {
+        // What is difference between Class And Object?
+        // What is difference between Abstract and Interface?
+        // How Try Catch block works? And how "Finally" works?
+        // What is dependency Injections  and how it helps and benefits?
+        // What is difference between REF and Out?
+        // Describe ASP.NET CORE?
+        // What is MVC?
+        // How Routing works in MVC Core? And How to define it?
+        // What is Startup class ASP.NET Core?
+        // What is host is ASP.NET Core?
+
+
+
+
+    }
+    internal class SQLQuestions
+    {
+        // How to select all Column in Customer Table
+        // How to join  tables
+
+
+
+    }
+}
